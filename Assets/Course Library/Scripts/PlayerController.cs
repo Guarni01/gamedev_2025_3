2b5e5623929ee2ad66d8a43431eb7acb50d7e6d1
Guarni01 <andreag.lunetta@gmail.com>
2025-10-31 13:59:25 +0100

    gamedev_2025_3 jump

---
+++ Assets/Course Library/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody body;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        body = GetComponent<Rigidbody>();
+        body.AddForce(Vector3.up * 1000);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
