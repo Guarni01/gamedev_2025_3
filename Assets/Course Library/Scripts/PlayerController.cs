e49fbada6ba8822dc35dea5b9e530b3117fab1bf
Guarni01 <andreag.lunetta@gmail.com>
2025-11-14 12:04:00 +0100

    sound + effects

--- Assets/Course Library/Scripts/PlayerController.cs
+++ Assets/Course Library/Scripts/PlayerController.cs
@@ -2,12 +2,26 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    //body + animation
     private Rigidbody body;
     public float jumpForce;
     public float gravityFactor;
     private bool isOnGround;
-    public bool isGameOver;
     private Animator animator;
+
+    //gamewise concepts
+    public bool isGameOver;
+
+    //suoni
+    public AudioClip crashSound;
+    public AudioClip jumpSound;
+    private AudioSource audioSource;
+
+    //FX
+    public ParticleSystem explosionParticles;
+    public ParticleSystem dirtParticles;
+    public AudioSource musicAudioSource;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -17,18 +31,22 @@ public class PlayerController : MonoBehaviour
         Physics.gravity *= gravityFactor;
         isOnGround = true;
         isGameOver = false;
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (isOnGround && Input.GetKeyDown(KeyCode.Space))
-            {
-                animator.SetTrigger("Jump_trig");
-                body.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-                isOnGround = false;
-            }
-                          
+        if (!isGameOver && isOnGround && Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.SetTrigger("Jump_trig");
+            body.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+            dirtParticles.Stop();
+            audioSource.PlayOneShot(jumpSound, 1.0f);
+
+        }
+
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -37,16 +55,23 @@ public class PlayerController : MonoBehaviour
         if (collidingObject.gameObject.CompareTag("Ground"))
         {
             isOnGround = true;
+            dirtParticles.Play();
         }
 
         else if (collidingObject.gameObject.CompareTag("Obstacles"))
         {
             isGameOver = true;
             animator.SetBool("Death_b", true);
+            int deathType = Random.Range(1, 3);
+            animator.SetInteger("DeathType_int", deathType);
             Debug.Log("GAMEOVER");
+            explosionParticles.Play();
+            dirtParticles.Stop();
+            audioSource.PlayOneShot(crashSound, 1.0f);
+            musicAudioSource.Stop();
         }
-        
-       
+
+
     }
-   
+
 }
